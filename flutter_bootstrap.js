00752aa593b7a7c12a7f3c1c85aee5ae8252cfed
ClaudRO <claudiorodriguez7778@gmail.com>
2025-09-24 18:31:27 -0300

    informe mxn

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2067999748"
+    serviceWorkerVersion: "1608372853"
   }
 });
