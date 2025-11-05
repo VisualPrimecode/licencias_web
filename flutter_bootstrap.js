fdc54e4f36806c90c4f1e8aebe43b6b48a761b7c
ClaudRO <claudiorodriguez7778@gmail.com>
2025-11-04 21:16:26 -0300

    act

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3708801170"
+    serviceWorkerVersion: "2377882395"
   }
 });
